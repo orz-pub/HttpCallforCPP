016df8e6302003f024f5b30533f65545500a10bb
bwv21 <jw.bwv21@gmail.com>
2018-11-24 12:29:26 +0900

    코드 정리 및 주석

--- src/HttpCallForCpp/HttpCallForCpp.cpp
+++ src/HttpCallForCpp/HttpCallForCpp.cpp
@@ -5,15 +5,17 @@
 
 #include "EasyWinHttp.h"
 
-#pragma warning(disable : 4996)
+#pragma warning(disable : 4996)		// ctime 워닝 끄기 위함.
 
 int main()
 {
 	std::wstring url = L"https://hooks.slack.com/services/..."; // slack webhook url.
 	std::wstring header = L"Content-Type: application/json";
+
+	// 실행할 때마다 username을 바꾸기 위해 현재시간을 사용(username을 바꿔야 테스트 확인하기 좋음).
 	std::time_t t = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-	std::string nowS = std::ctime(&t);
-	std::wstring now(nowS.begin(), nowS.end());
+	std::string ctime = std::ctime(&t);
+	std::wstring now(ctime.begin(), ctime.end());
 
 	std::wstring data = L"{"
 		LR"("username": ")" + now + LR"(",)"
@@ -25,12 +27,12 @@ int main()
 			LR"({"text": "<https://google.com|link(english only)>"})"
 		LR"(],)"
 		LR"("mkdown": true)"
-						L"}";
+	L"}";
 
 	EasyWinHttp easyWinHttp;
 	std::string result = easyWinHttp.Post(url, header, data);
 
-	std::cout << "===== result =====" << std::endl;
+	std::cout << "================== result ==================" << std::endl;
 	std::cout << result << std::endl;
-	std::cout << "==================" << std::endl;
+	std::cout << "============================================" << std::endl;
 }
